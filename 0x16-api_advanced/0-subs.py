33f15a61b4fd30364740917607fe981b3c4a1055
Amna2011 <amnataha2011@gmail.com>
2023-11-07 18:30:47 -0800

    mmm

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,16 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'YourApp/1.0'}
-
-    # Make a GET request to the Reddit API
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=headers)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        data = response.json()
-        # Extract the number of subscribers
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        # Invalid subreddit or other error
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "underscoDe@alx-holbertonschool"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-
-# Example usage:
-subreddit_name = 'learnpython'
-subscribers = number_of_subscribers(subreddit_name)
-if subscribers > 0:
-    print(f'The subreddit r/{subreddit_name} has {subscribers} subscribers.')
-else:
-    print(f'The subreddit r/{subreddit_name} is not valid.')
+    results = response.json().get("data")
+    return results.get("subscribers")
